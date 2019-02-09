50a1a1aeadabd8789486a6cf427529d314fc347c
Vamshinath <vamshinath.m8@gmail.com>
2019-02-09 22:45:06 +0530

    v1

--- mainApp.py
+++ mainApp.py
@@ -1,6 +1,6 @@
 #!/usr/python3
 import os,sys
-
+import magic
 def getDirSizeAndFiles(path):
 
     sz = 0
@@ -17,39 +17,82 @@ def getDirSizeAndFiles(path):
     files = sorted(files.items(),key=lambda x:x[1],reverse=True)
     return [sz,files]
 
-def scanFiles(scanOption):
+def scanFiles(path):
 
     files={}
     dirs={}
+    outfiles={}
+    print("in dir:"+path)
+    os.chdir(path)
+  
+    for fl in os.listdir("."):
+        if os.path.isdir(fl):
+            dirs[fl]=getDirSizeAndFiles(fl)
+        else:
+            outfiles[fl]=os.stat(fl).st_size
 
-    if scanOption == 'r':
-        for root,drs,fls in os.walk("."):
-            for fl in fls:
-                try:
-                    fullpathfl=os.path.abspath(os.path.join(root,fl))
-                    print(fullpathfl)
-                    files[fullpathfl]=os.stat(fl).st_size
-                except Exception as e:
-                    k=0
-    else:
-        for fl in os.listdir("."):
-            if os.path.isdir(fl):
-                dirs[fl]=getDirSizeAndFiles(fl)
-            else:
-                files[fl]=os.stat(fl).st_size
 
     sortedDirs = sorted(dirs.items(),key=lambda x:x[1][0],reverse=True)
+    sortedOutFiles = sorted(outfiles.items(),key=lambda x:x[1],reverse=True)
 
+    outerctr=0
+
+    
+
+    printDirs={}
+
+    index=1
     for dr,sz in sortedDirs:
-        print(dr,sz[0])
+        outerctr+=1
+        if outerctr == 10:
+            break
+        print(str(index)+" "+dr+" "+str(sz[0]))
+        printDirs[index]=dr
+        index+=1
         ctr=0
         for fl,flsz in sz[1]:
-            print("\t"+fl+" "+str(flsz))
+            print("\t"+str(index)+" "+fl+" "+str(flsz))
+            printDirs[index]=fl
+            index+=1
             ctr+=1
             if ctr == 10:
                 break
-        
-            
+    ctr=0
+    for otfl,otsz in sortedOutFiles:
+        print("\t"+str(index)+" "+otfl+" "+str(otsz))
+        printDirs[index]=otfl
+        index+=1
+        ctr+=1
+        if ctr == 10:
+            break
+
+    option = int(input("Enter option:"))
+
+    if option == 0:
+        return ".."
+
+
+
+    selectedOption=printDirs[option]
+
+    if os.path.isdir(selectedOption):
+        print("returning "+selectedOption)
+        return selectedOption
+    else:
+        playMedia(selectedOption)
+    
+    return ""
+
+def playMedia(fl):
+    ext = magic.from_file(fl,mime=True).split('/')[-1]
+
+    if "jpeg" == ext or "png" ==ext or "gif" == ext or "jpg" == ext:
+        os.system("pqiv -c -f '"+fl+"' ")
+    else:
+        os.system("vlc '"+fl+"' ")
+    return
+
+
 
 
 def main():
@@ -61,7 +104,13 @@ def main():
 
     os.chdir(sys.argv[1])
 
-    scanFiles(input("Enter option baseScan or recursive Scan(r/*):"))
+    path="."
+    while True:
+        drfl=scanFiles(path)
+        if drfl == "":
+            path="."
+        else:
+            path=os.path.abspath(os.path.join(os.getcwd(),drfl))
 
 
 
